3e25be5a77b4f8ad93c4d961ea3e356edc87654e
etprogrammer <extraterrestrialprogrammer@gmail.com>
2023-01-29 17:57:52 +1100

    Add library target name

--- Package.swift
+++ Package.swift
@@ -9,7 +9,8 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ConfidentialTechnologyPackage-ETP",
-            targets: ["ConfidentialTechnology"]),
+            targets: ["ConfidentialTechnology"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
